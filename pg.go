554d13af76254f6526ef73528088c3352d110610
Steinar V. Kaldager <steinarvkaldager@gmail.com>
2018-10-23 01:27:35 +0100

    add output as option strings
    as used by golang pq package

--- pg.go
+++ pg.go
@@ -17,6 +17,23 @@ type PostgresOptions struct {
 	WithoutSSL bool
 }
 
+func (d *Postgres) check() error {
+	switch {
+	case d.Host == "":
+		return errors.New("missing host")
+	case d.Port == 0:
+		return errors.New("missing port")
+	case d.Database == "":
+		return errors.New("missing database")
+	case d.User == "":
+		return errors.New("missing user")
+	case d.Password == "":
+		return errors.New("missing password")
+	default:
+		return nil
+	}
+}
+
 func (d *Postgres) AsURLWithOptions(opts *PostgresOptions) (string, error) {
 	if opts == nil {
 		opts = &PostgresOptions{}
@@ -27,22 +44,19 @@ func (d *Postgres) AsURLWithOptions(opts *PostgresOptions) (string, error) {
 		optionstring = "?ssl=true"
 	}
 
-	switch {
-	case d.Host == "":
-		return "", errors.New("missing host")
-	case d.Port == 0:
-		return "", errors.New("missing port")
-	case d.Database == "":
-		return "", errors.New("missing database")
-	case d.User == "":
-		return "", errors.New("missing user")
-	case d.Password == "":
-		return "", errors.New("missing password")
-	default:
-		return fmt.Sprintf("postgres://%s:%s@%s:%d/%s%s", d.User, d.Password, d.Host, d.Port, d.Database, optionstring), nil
+	if err := d.check(); err != nil {
+		return "", err
 	}
+	return fmt.Sprintf("postgres://%s:%s@%s:%d/%s%s", d.User, d.Password, d.Host, d.Port, d.Database, optionstring), nil
 }
 
 func (d *Postgres) AsURL() (string, error) {
 	return d.AsURLWithOptions(nil)
+}
+
+func (d *Postgres) AsOptionString() (string, error) {
+	if err := d.check(); err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("host=%s port=%d dbname=%s user=%s password=%s sslmode=require", d.Host, d.Port, d.Database, d.User, d.Password), nil
 }
